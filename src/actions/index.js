a08315142f54b7035a5b904f8b3979fd250f4d36
yuhsing <bob301833@gmail.com>
2017-06-08 09:57:51 +0800

    install axios and fetchWeather Action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,2 +1,17 @@
-const API_KEY = '9e04e79b30b562091e8eaf7f8d725fce';
+import axios from 'axios';
 
+
+const API_KEY = '9e04e79b30b562091e8eaf7f8d725fce';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export const fetchWeather = (city) => {
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+};
